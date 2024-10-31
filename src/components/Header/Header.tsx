34d33366a4b4a281b9bb781f73e6932bdc895214
jeyson08 <jeyson.boursault@gmail.com>
2024-10-31 18:46:58 +0100

    feat: header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex justify-center items-center h-[100px]">
+      <h1>Compound</h1>
+    </div>
+  );
+};
+
+export default Header;
